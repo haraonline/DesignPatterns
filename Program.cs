a2a266f0e28643afe2be6b5a739e672da83f5202
Hara Gopal Kovvali <HaraGopal.Kovvali@ece.com>
2023-02-22 17:02:10 +0100

    start SOLID

--- Program.cs
+++ Program.cs
@@ -1,10 +1,32 @@
-﻿namespace DesignPatterns
+﻿using System.ComponentModel.Design;
+using System.Reflection.Emit;
+
+namespace DesignPatterns
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Design Patterns");
+
+            /// <summary>
+            ///SOLID is an acronym for five design principles in software development that are intended to help make code more flexible, maintainable, and easy to understand.
+            ///S - Single Responsibility Principle: A class should have only one reason to change.This means that each class should have a single, clear responsibility that it performs well.
+            ///O - Open/Closed Principle: A software module should be open for extension but closed for modification.This means that existing code should be protected from changes but should be able to be extended to add new functionality.
+            ///L - Liskov Substitution Principle: A subclass should be able to be used in place of its parent class without any problems.This means that derived classes should be able to be substituted for their base classes without breaking the program.
+            ///I - Interface Segregation Principle: A client should not be forced to implement interfaces they do not use. This means that interfaces should be designed to be as small and specific as possible.
+            ///D - Dependency Inversion Principle: High-level modules should not depend on low-level modules. Both should depend on abstractions.This means that code should be organized in such a way that high-level code is independent of low-level details, which can help make the code more modular and easier to maintain.
+            ///
+            /// Overall, these principles help to ensure that code is designed in a way that is flexible, maintainable, and easy to understand, which can help reduce bugs and make development more efficient.
+            /// </summary>/// 
+
+
+            ShoppingList lst = new ShoppingList();
+            lst.AddItem("Apple");
+            lst.AddItem("Banana");
+            lst.AddItem("Oranges");
+
+            Console.WriteLine(lst);           
+
         }
     }
 }
---
+++ ShoppingList.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DesignPatterns
+{
+    internal class ShoppingList
+    {
+        //variables
+        int count = 0;
+
+        //properties
+        public List<string> LebensmittelListe { get; set; } = new List<string>();
+
+        //methods
+        public void AddItem(string item)
+        {
+            LebensmittelListe.Add((count++) + " : " + item);
+            
+        }
+
+        public void RemoveItem(string item)
+        {
+            LebensmittelListe.Remove((count--) + " : " + item);            
+        }
+
+        public override string ToString()
+        {
+            string output = "";
+            foreach (var item in LebensmittelListe)
+            {
+                output += item + Environment.NewLine;
+            }
+            return output;
+        }
+    }
+}
